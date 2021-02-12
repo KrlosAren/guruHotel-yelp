dcd547cacd599aea6ad386ae65f3d65d22453bd3
Carlos Lopez Zavarce <56363348+KrlosAren@users.noreply.github.com>
2021-02-12 12:20:08 -0300

    Update index.js

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -4,13 +4,16 @@ const API_KEY =
   'Fz2Ih6jEB9oJ2PEAIMTE3RWX16vqG1X9yHXIDZjEz809xmu0habmkR1Fh-93WeFZAnhFxXLzJgeJVOzHqjeS4lOITfAztpsdnPoN87a82Gmtpd955YSv5hcMrxYaYHYx';
 
 const client = new ApolloClient({
-  uri: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql',
+  uri: 'https://api.yelp.com/v3/graphql',
   cache: new InMemoryCache(),
   credentials: 'same-origin',
   headers: {
     Authorization: `Bearer ${API_KEY}`,
     'Accept-Language': 'en_US',
   },
+  fetchOptions: {
+    mode: 'no-cors',
+  }
 });
 
 export default client;
